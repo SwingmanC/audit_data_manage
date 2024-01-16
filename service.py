7fe3e8c2acc9650f54073bf4f31523eab2983122
jsfnsc <12557314678@qq.com>
2024-01-16 16:35:45 +0800

    20240116

--- service.py
+++ service.py
@@ -42,11 +42,11 @@ def upload_vault_reason(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][1]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'SERVNUMBER, COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6, COLUMN7, COLUMN8, COLUMN9, COLUMN10, COLUMN11, COLUMN12, COLUMN13,'
                                'SHOW_ID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21,:22,:23,:24,:25)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21,:22,:23,:24,:25)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, str(staff_id),rec_date,
                                   item.op_tele, '金库前台异常申请理由',item.op_id, item.op_name, item.op_org, item.op_path, item.reason, item.op_time, item.app_name,item.app_id, item.app_tele,item.app_org,item.app_path, item.app_time,
                                   item.show_id, 0, 1))
@@ -71,11 +71,11 @@ def upload_bes_pljk(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3'
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE,'
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE,'
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6,COLUMN7,COLUMN8,'
                                'SHOW_ID, RECDEFID, einvoice_flag, cd_flag)'
                                'values'
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   'bes批量监控审计',item.username, item.zh_name,item.id_state,item.org_path,item.org,item.zh_type,item.query_cnt,
                                   item.show_id, item.type, 0, 1))
@@ -100,11 +100,11 @@ def upload_bes_ycsj(db_connect, file_path, date,log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6, COLUMN7, COLUMN8, COLUMN9, COLUMN10,'
                                'SHOW_ID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   'bes人员异常操作业务数据与全天占比超70%审计',item.username, item.zh_name,item.id_state,item.org_path, item.org, item.time, item.op_cnt, item.op_sum, item.per,
                                   item.show_id, 0, 1))
@@ -129,11 +129,11 @@ def upload_bes_dsj(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6, COLUMN7, COLUMN8,COLUMN9,'
                                'SHOW_ID,RECDEFID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   '异常监控每人每个业务1分钟内大于3次汇总',item.username, item.zh_name, item.id_state,item.org_path,item.org,item.zh_type,item.time,item.cnt,
                                   item.show_id, item.type, 0, 1))
@@ -158,11 +158,11 @@ def upload_bes_sum(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6, COLUMN7,COLUMN8,'
                                'SHOW_ID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   '苏州异常时间过户、开户、补卡、复机汇总',item.username, item.zh_name, item.id_state,item.org_path, item.org, item.time, item.cnt,
                                   item.show_id, 0, 1))
@@ -185,11 +185,11 @@ def upload_pw_detect_detail(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6, COLUMN7, COLUMN8, COLUMN9, COLUMN10, COLUMN11, COLUMN12, COLUMN13, COLUMN14, COLUMN15,'
                                'SHOW_ID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21,:22,:23,:24,:25,:26)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21,:22,:23,:24,:25,:26)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   '密码探测登录失败明细统计',item.db, item.staff_name, item.org, item.mobile, item.ip, item.op_time, item.pw, item.cydl, item.dcn, item.fhtz, item.interval, item.op_cnt, item.repeat_per, item.remark,
                                   item.show_id, 0, 1))
@@ -210,11 +210,11 @@ def upload_pw_detect_sum(db_connect, file_path, date, log_file_path):
             org_id = recorg_id_list[0][0]
             rec_date = datetime.datetime.now()
             cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                           '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                           '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                            'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6, COLUMN7, '
                            'SHOW_ID,einvoice_flag,cd_flag)'
                            'values '
-                           '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18)'
+                           '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18)'
                            ,(int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                               '全省前台客户服务密码探测表', item.org,  item.staff_name, item.op_time, item.op_cnt, item.number_cnt, item.remark,
                               item.show_id, 0, 1))
@@ -237,11 +237,11 @@ def upload_order_query(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5,COLUMN6,COLUMN7, COLUMN8, COLUMN9, COLUMN10, COLUMN11,'
                                'SHOW_ID,RECDEFID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21,:22,:23)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21,:22,:23)'
                                ,(int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   '单个清单查询', item.op_time, item.zh_name, item.username, item.op_path,item.op_org,item.op_res,item.start_time, item.end_time, item.ob_id, item.telephone,
                                   item.show_id, item.order_type, 0, 1))
@@ -264,11 +264,11 @@ def upload_sst_query(db_connect, file_path, date, log_file_path):
                 org_id = recorg_id_list[0][0]
                 rec_date = datetime.datetime.now()
                 cursor.execute('insert into qdyy_data.JH_BUSI_INFO@szyy3 '
-                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, RECORGID, RECOPID, RECDATE, '
+                               '(id, GROUPID, REGION_ID, BUSI_ID, RULE_ID, EXEC_DATE, RECORGID, RECOPID, RECDATE, '
                                'COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5, COLUMN6,'
                                'SHOW_ID,einvoice_flag,cd_flag)'
                                'values '
-                               '(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17)'
+                               '(:1,:2,:3,:4,:5,to_char(sysdate-1,\'yyyymmdd\'),:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17)'
                                , (int(item.id), item.group_id, item.region_id, item.busi_id, item.rule_id, org_id, item.staff_id, rec_date,
                                   '首三推查询日志', item.oper_time, item.zh_name, item.telephone, item.org, item.op_time,
                                   item.show_id, 0, 1))
